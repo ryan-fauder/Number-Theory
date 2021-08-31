d883f1121d81135ee6787057d5d50323116f4a89
Ryan Auder <ryan.fauder@gmail.com>
2021-08-31 18:09:21 -0300

    testing: unit tests for gcd_recursive

--- Greatest Common Divisor/main.cpp
+++ Greatest Common Divisor/main.cpp
@@ -2,6 +2,9 @@
 #include "Euclides-Algorithm.hpp"
 using namespace std;
 int main(){
-  cout << gcd_recursive(13, 1);
+  cout << gcd_recursive(32, 6) << endl; // it must be 2
+  cout << gcd_recursive(1232, 200) << endl; // it must be 8
+  cout << gcd_recursive(30, 165) << endl; // it must be 15
+  cout << gcd_recursive(2, 3) << endl; // it must be 1
   return 0;
 }
