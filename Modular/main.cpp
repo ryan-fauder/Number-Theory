66a9de0b2dad14f1d8ac0fcfb9fc1c0020be8db9
Ryan Auder <ryan.fauder@gmail.com>
2021-08-31 22:44:08 -0300

    fix: Some Not ASCII caracters

--- Modular/main.cpp
+++ Modular/main.cpp
@@ -9,7 +9,7 @@ int main()
   cout << multiplyModular(15, 3) << endl;
   cout << subModular(15, 3) << endl;
   // n = 70450
-  // ((180 sub 6801) ⊗ 12459) ⊗ (30260 ⊕ 70441 sub 967)
+  // ((180 sub 6801) mult 12459) mult (30260 sum 70441 sub 967)
   modularClass::setNumber(70450);
   int first = multiplyModular(subModular(180, 6801), 12459);
   int second = subModular(sumModular(30260, 70441), 967);
